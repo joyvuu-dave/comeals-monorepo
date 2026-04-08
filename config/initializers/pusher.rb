5f8c2a4d264ccbfe157e3b887e2eedfa1449aa87
David Riddle <david@joyvuu.com>
2026-04-08 16:57:58 -0500

    Move Pusher credentials from hard-coded values to environment variables
    Production values are set as Heroku config vars; local dev uses .env
    file loaded by dotenv-rails.

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -2,9 +2,12 @@
 
 require 'pusher'
 
-Pusher.app_id = '371753'
-Pusher.key = '8affd7213bb4643ca7f1'
-Pusher.secret = 'd913e03184efc1b7458f'
-Pusher.cluster = 'us2'
+# Pusher credentials for real-time WebSocket push notifications.
+# Production: Heroku config vars (PUSHER_APP_ID, PUSHER_KEY, PUSHER_SECRET, PUSHER_CLUSTER)
+# Local dev:  .env file (loaded by dotenv-rails)
+Pusher.app_id = ENV.fetch('PUSHER_APP_ID')
+Pusher.key = ENV.fetch('PUSHER_KEY')
+Pusher.secret = ENV.fetch('PUSHER_SECRET')
+Pusher.cluster = ENV.fetch('PUSHER_CLUSTER')
 Pusher.logger = Rails.logger
 Pusher.encrypted = true
